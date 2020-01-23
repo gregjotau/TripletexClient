cd3938757be2ce296b7f555017539893724fe349
Greg Taube <gregjotau@gmail.com>
2020-01-23 14:28:57 +0100

    adding config

--- generate_tt_sdk.sh
+++ generate_tt_sdk.sh
@@ -1,4 +1,4 @@
-swagger-codegen generate -i http://tripletex.no/v2/swagger.json -l python -o ./tt_client
+swagger-codegen generate -i http://tripletex.no/v2/swagger.json -l python -c ./config.json -o ./tt_client
 
 cd tt_client
 python setup.py install
